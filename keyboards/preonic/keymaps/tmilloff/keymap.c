073e29762ff71121737f47e6c1298d4f4721e166
Jason Milloff <tmilloff@radford.edu>
2021-02-23 13:40:15 -0500

    Add capslock key and layer for typing with one hand

--- keyboards/preonic/keymaps/tmilloff/keymap.c
+++ keyboards/preonic/keymaps/tmilloff/keymap.c
@@ -32,7 +32,9 @@ enum preonic_layers {
   _RAISE,
   _GUI,
   _MACRO,
-  _ADJUST
+  _ADJUST,
+  _ONEHANDBASE,
+  _ONEHANDFLIP,
 };
 
 enum preonic_keycodes {
@@ -123,9 +125,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_GUI] = LAYOUT_preonic_grid(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
   _______, _______, _______, MAC_CAPTURE_SCREEN, MAC_COPY_PORTION, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+  KC_CAPS, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
   _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, _______,
-  MO(_MACRO), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, MO(_MACRO)
+  MO(_MACRO), _______, _______, _______, _______, TG(_ONEHANDBASE), TG(_ONEHANDBASE), _______, _______, _______, _______, MO(_MACRO)
 ),
 
 /* MACRO
@@ -168,9 +170,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
-)
+),
 
+/* One hand (Based on qwerty)
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | GUI  | Ctrl | Alt  |  OS  |Lower |    Space    |Raise |  OS  | Alt  | Ctrl | GUI  |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_ONEHANDBASE] = LAYOUT_preonic_grid( \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RCTL_T(KC_QUOT),
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
+  TG(_ONEHANDBASE), KC_LCTL, KC_LALT, KC_LGUI, KC_SPACE,   LT(_ONEHANDFLIP, KC_SPACE), LT(_ONEHANDFLIP, KC_SPACE),  KC_SPACE,   KC_RGUI, KC_RALT, KC_RCTL, TG(_ONEHANDBASE)
+),
 
+/* One hand (Based on qwerty)
+ * ,-----------------------------------------------------------------------------------.
+ * |  Del |  0   |  9   |  8   |  7   |  6   |  5   |  4   |  3   |  2   |  1   |  `   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Bksp |  P   |  O   |  I   |  U   |  Y   |  T   |  R   |  E   |  W   |  Q   |  Tab |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |   "  |  ;   |  L   |  K   |  J   |  H   |  G   |  F   |  D   |  S   |  A   |  Esc |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Enter|  /   |  .   |  ,   |  M   |  N   |  B   |  V   |  C   |  X   |  Z   |Shift |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_ONEHANDFLIP] = LAYOUT_preonic_grid( \
+  KC_DEL, KC_0, KC_9, KC_8, KC_7, KC_6, KC_5, KC_4, KC_3, KC_2, KC_1, KC_GRV,
+  KC_BSPC, KC_P, KC_O, KC_I, KC_U, KC_Y, KC_T, KC_R, KC_E, KC_W, KC_Q, KC_TAB,
+  RCTL_T(KC_QUOT), KC_SCLN, KC_L, KC_K, KC_J, KC_H, KC_G, KC_F, KC_D, KC_S, KC_A, LCTL_T(KC_ESC),
+  RSFT_T(KC_ENT), KC_SLSH, KC_DOT, KC_COMM, KC_M, KC_N, KC_B, KC_V, KC_C, KC_X, KC_Z, KC_LSFT,
+  _______, _______, _______, _______, _______,   _______,  _______,  _______,   _______, _______, _______, _______
+),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
